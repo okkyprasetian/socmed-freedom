52eb271adf54df08f6fd8eb9e1fce90db3d7f1d9
okkyprasetian <okkyprasetian@gmail.com>
2022-04-02 07:10:04 +0700

    finish baisc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 function App() {
   return (
     <div className="App">
+      <h1>Social Media Freedom</h1>
+      <p>
+        Seperti teori dari vsauce, waktu kita relative terhadap pengalaman yg kita alami dan yg sudah kita alami. Ada alasan mengapa waktu kita kecil, waktu berjalan seperti sangat lambat, sedangkan semakin dewasa maka waktu berjalan semakin cepat.
+      </p>
+
+      <p>
+        Seperti saat aku menonton anime. Anime pertamaku, aku begitu mengingatnya, bahkan tiap episode. Charlote yg hanya 13 episode terasa lebih lama daripada FMA yg lebih dari 50 episode. Inilah time pill yg lebih kejam daripada pill pill lainnya.
+      </p>
+
+      <p>
+        Masa mudaku hanya bertahan tidak lama. Jika tidak aku manfaatkan dan nikmati. Aku akan menghabiskan masa tuaku dengan menderita dan waktu berjalan begitu cepat. Masa remajaku tidak aku nikmati, aku tidak ingin masa mudaku juga tidak aku nikmati.
+      </p>
+
+      <p>
+        Internetan seharian, binge youtube, porn, membuka forum online berjam jam. Apa ini yg aku ingin lakukan di masa mudaku?
+      </p>
+
+      <p>
+        Aku ingin menjadi athlete fencer nasional dan menjadi ethical hacker. Inilah dua hal yg perlu aku jalani dengan disiplin di masa mudaku.
+      </p>
     </div>
   );
 }
